2de8fbeae1f9372366786132b8544ee440b4ff68
DarkMatter-999 <darkmatter999official@gmail.com>
2024-08-03 22:17:04 +0530

    Added Geolocation markers from latitude and longitude

--- water_supply_management/water_supply_management/doctype/water_meter/water_meter.js
+++ water_supply_management/water_supply_management/doctype/water_meter/water_meter.js
@@ -1,8 +1,32 @@
 // Copyright (c) 2024, DarkMatter-999 and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Water Meter", {
-// 	refresh(frm) {
+frappe.ui.form.on('Water Meter', {
+    meter_latitude: function(frm) {
+        update_marker(frm);
+    },
+    meter_longitude: function(frm) {
+        update_marker(frm);
+    }
+});
 
-// 	},
-// });
+function update_marker(frm) {
+    if (frm.doc.meter_latitude && frm.doc.meter_longitude) {
+        var latitude = parseFloat(frm.doc.meter_latitude);
+        var longitude = parseFloat(frm.doc.meter_longitude);
+        
+        var geojson = {
+            type: "FeatureCollection",
+            features: [{
+                type: "Feature",
+                properties: {},
+                geometry: {
+                    type: "Point",
+                    coordinates: [longitude, latitude]
+                }
+            }]
+        };
+        
+        frm.set_value('meter_coordinates', JSON.stringify(geojson));
+    }
+}
